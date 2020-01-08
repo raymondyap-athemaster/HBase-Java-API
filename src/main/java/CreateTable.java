e787780616b0b89f332586072d73208b0d7e2e0f
raymondyap-athemaster <raymond.yap@athemaster.com>
2020-01-08 14:40:23 +0800

    try using with old api

--- src/main/java/CreateTable.java
+++ src/main/java/CreateTable.java
@@ -21,25 +21,22 @@ public class CreateTable
         conf.set("hbase.zookeeper.property.clientPort","2181");
         conf.set("hbase.master", "35.229.149.35:16010");
 
-        // Instantiating HbaseAdmin class
-        //con.set();
-        //HBaseAdmin admin = new HBaseAdmin(con);
-        Connection conn = ConnectionFactory.createConnection(conf);
-        Admin admin = conn.getAdmin();
+        // Instantiating configuration class
+        Configuration con = HBaseConfiguration.create();
 
-        TableName tableName = TableName.valueOf("emp2");
-        //表描述器构造器
-        TableDescriptorBuilder  tdb  =TableDescriptorBuilder.newBuilder(tableName)  ;
-        //列族描述起构造器
-        ColumnFamilyDescriptorBuilder cdb =  ColumnFamilyDescriptorBuilder.newBuilder(Bytes.toBytes("personal data"));
-        //获得列描述起
-        ColumnFamilyDescriptor  cfd = cdb.build();
-        //添加列族
-        tdb.setColumnFamily(cfd);
-        //获得表描述器
-        TableDescriptor td = tdb.build();
-        //创建表
-        //admin.addColumnFamily(tableName, cfd); //给标添加列族
-        admin.createTable(td);
+        // Instantiating HbaseAdmin class
+        HBaseAdmin admin = new HBaseAdmin(conf);
+
+        // Instantiating table descriptor class
+        HTableDescriptor tableDescriptor = new
+                HTableDescriptor(TableName.valueOf("emp"));
+
+        // Adding column families to table descriptor
+        tableDescriptor.addFamily(new HColumnDescriptor("personal"));
+        tableDescriptor.addFamily(new HColumnDescriptor("professional"));
+
+        // Execute the table through admin
+        admin.createTable(tableDescriptor);
+        System.out.println(" Table created ");
     }
 }
