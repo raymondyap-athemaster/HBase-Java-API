5ae0b05b667880aa73dbb7c5223478a4e7dd3351
raymondyap-athemaster <raymond.yap@athemaster.com>
2020-01-08 17:11:20 +0800

    Add List Table

--- src/main/java/ListTable.java
+++ src/main/java/ListTable.java
@@ -1,3 +1,4 @@
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.Connection;
@@ -7,35 +8,29 @@ import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
 
-public class DeleteTable
-{
 
-    public static Configuration configuration;
-    public static Connection connection;
-    public static Admin admin;
+public class ListTable
+{
+    private static Connection connection;
+    private static Configuration configuration;
+    private static Admin admin;
 
     public static void main(String[] args) throws IOException
     {
         System.out.println("Tested.\n");
 
         configuration = HBaseConfiguration.create();
-        //configuration.setConfiguration("hbase.rootdir","hdfs://idx046:9000/hbase");
         configuration.set("hbase.zookeeper.quorum","35.229.149.35");
         configuration.set("hbase.zookeeper.property.clientPort","2181");
         configuration.set("hbase.master", "35.229.149.35:16010");
 
         connection = ConnectionFactory.createConnection(configuration);
-        admin = connection.getAdmin();
-
-        Table table = connection.getTable(TableName.valueOf("test"));
-
-        connection = ConnectionFactory.createConnection(configuration);
-
-
-        TableName tableName = TableName.valueOf("emp2");
-        admin.disableTable(tableName);
-        admin.deleteTable(tableName);
-        System.out.println("Table deleted.\n");
+        admin =connection.getAdmin();
+        HTableDescriptor[] tableDescriptor = admin.listTables();
+        // printing all the table names.
+        for (HTableDescriptor hTableDescriptor : tableDescriptor) {
+            System.out.println(hTableDescriptor.getNameAsString());
+        }
 
     }
 }
