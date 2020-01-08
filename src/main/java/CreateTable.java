98afef7379d6ba7d97f59a65d2d927ff85c34e5d
raymondyap-athemaster <raymond.yap@athemaster.com>
2020-01-08 15:21:44 +0800

    local and global variable

--- src/main/java/CreateTable.java
+++ src/main/java/CreateTable.java
@@ -1,27 +1,28 @@
 import java.io.IOException;
 
-
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.TableName;
 
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.util.Bytes;
 
 public class CreateTable
 {
+    static Configuration conf = null;
+    static Connection conn = null;
     public static void main(String[] args) throws IOException
     {
         // Instantiating configuration class
-        Configuration conf = HBaseConfiguration.create();
+        conf = HBaseConfiguration.create();
 
         conf.set("hbase.zookeeper.quorum","35.229.149.35");
         conf.set("hbase.zookeeper.property.clientPort","2181");
         conf.set("hbase.master", "35.229.149.35:16010");
 
-        Connection conn = ConnectionFactory.createConnection(conf);
+        conn = ConnectionFactory.createConnection(conf);
         Admin admin = conn.getAdmin();
         TableName tableName = TableName.valueOf("emp2");
         TableDescriptorBuilder  tdb  =TableDescriptorBuilder.newBuilder(tableName);
