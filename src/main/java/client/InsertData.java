26f476f89207afe51e8b7b68eac6c36783c7a9e3
raymondyap-athemaster <raymond.yap@athemaster.com>
2020-01-08 17:57:17 +0800

    Add insert Data

--- src/main/java/client/InsertData.java
+++ src/main/java/client/InsertData.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
 
@@ -25,6 +26,28 @@ public class InsertData
 
         connection = ConnectionFactory.createConnection(configuration);
 
+        // Instantiating HTable class
+        HTable hTable = new HTable(configuration, args[0]);
 
+        // Instantiating Put class
+        // accepts a row name.
+        Put p = new Put(Bytes.toBytes("row1"));
+
+        // adding values using add() method
+        // accepts column family name, qualifier/row name ,value
+        p.add(Bytes.toBytes("personal"), Bytes.toBytes("name"),Bytes.toBytes("raju"));
+
+        p.add(Bytes.toBytes("personal"), Bytes.toBytes("city"),Bytes.toBytes("hyderabad"));
+
+        p.add(Bytes.toBytes("professional"),Bytes.toBytes("designation"), Bytes.toBytes("manager"));
+
+        p.add(Bytes.toBytes("professional"),Bytes.toBytes("salary"), Bytes.toBytes("50000"));
+
+        // Saving the put Instance to the HTable.
+        hTable.put(p);
+        System.out.println("data inserted");
+
+        // closing HTable
+        hTable.close();
     }
 }
